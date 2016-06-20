a329a1c2318822344e230a4f0e3ad348db7c8596
Mario Aliaga <marioaliaga79@gmail.com>
2016-06-20 16:15:45 -0400

    Agrego region y cambio de EJS a Handlerbars

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,7 +39,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('common/error', {
       message: err.message,
       error: err
     });
