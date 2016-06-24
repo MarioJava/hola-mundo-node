cad591bdb56c7f0c6cb2b6c9a7f92f27dd91e41f
Mario Aliaga <marioaliaga79@gmail.com>
2016-06-24 00:06:11 -0400

    Agrego Enrutamiento para lista y detalles de los articulos

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,22 @@ EventManager.addRegions({
     mainRegion: "#main-region"
 });
 
-EventManager.on("start", function () {
-    EventManager.EventosApp.List.Controller.listEventos();
+EventManager.navigate = function(route, options){
+    options || (options = {});
+
+    Backbone.history.navigate(route, options);
+};
+
+EventManager.getCurrentRoute = function(){
+    return Backbone.history.fragment
+};
+
+EventManager.on("start", function(){
+    if (Backbone.history) {
+        Backbone.history.start();
+
+        if(this.getCurrentRoute() === ""){
+            EventManager.trigger("events:list");
+        }
+    }
 });
--- public/js/apps/eventos/show/show_controller.js
+++ public/js/apps/eventos/show/show_controller.js
@@ -3,10 +3,17 @@ EventManager.module("EventosApp.Show", function(Show,
 
     Show.Controller = {
 
-        showEvento: function (model) {
+        showEvento: function (id) {
+            var eventos = EventManager.request("eventos:entities");
+
+            var model = eventos.get(id);
 
             var eventoView = new Show.Event({
                 model: model
+            });
+
+            eventoView.on("showList:eventos", function(model){
+                EventManager.trigger("events:list");
             });
 
             EventManager.mainRegion.show(eventoView);
